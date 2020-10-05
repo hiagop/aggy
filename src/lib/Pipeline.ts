2c658d976cadc0b861bb81ff2833d13ddd6b4566
Hiago Prata <hcprata17@gmail.com>
2020-10-04 21:19:48 -0300

    feat: add Pipeline class and basic methods

---
+++ src/lib/Pipeline.ts
@@ -0,0 +1,77 @@
+import { StageError } from "./Errors";
+
+export class Pipeline {
+  private _pipeline: PipelineStage[] = [];
+
+  get pipeline() {
+    return this._pipeline;
+  }
+
+  constructor() {}
+
+  skip(n: number) {
+    if (!Number.isInteger(n)) {
+      throw new StageError("Only integer values are allowed.");
+    }
+
+    if (n < 0) {
+      throw new StageError("Only non negative integers are allowed.");
+    }
+
+    const stage = { $skip: n };
+
+    this._pipeline.push(stage);
+
+    return this;
+  }
+
+  limit(n: number) {
+    if (!Number.isInteger(n)) {
+      throw new StageError("Only integer values are allowed.");
+    }
+
+    if (n < 0) {
+      throw new StageError("Only positive integers are allowed.");
+    }
+
+    const stage = { $limit: n };
+
+    this._pipeline.push(stage);
+
+    return this;
+  }
+
+  sort(...args: SortProperty[]) {
+    const stage = { $sort: {} };
+
+    Object.assign(stage.$sort, ...args);
+
+    this._pipeline.push(stage);
+
+    return this;
+  }
+
+  match(...args: MatchAttribute[]) {
+    const stage: PipelineStage = { $match: {} };
+
+    Object.assign(stage.$match, ...args);
+
+    this._pipeline.push(stage);
+
+    return this;
+  }
+}
+
+type MatchAttribute = { [key: string]: any };
+
+type SortProperty = { [key: string]: 1 | -1 };
+
+type PipelineOperator =
+  | "$match"
+  | "$skip"
+  | "$limit"
+  | "$sort";
+
+type PipelineStage = {
+  [key in PipelineOperator]?: any;
+};
