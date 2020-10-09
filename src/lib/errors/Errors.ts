ebad8a5d16ac34887b33ea012f5ad2f44e53a7d7
Hiago Prata <hcprata17@gmail.com>
2020-10-09 13:05:28 -0300

    feat: create error codes factory

--- src/lib/errors/Errors.ts
+++ src/lib/errors/Errors.ts
@@ -3,3 +3,16 @@ class StageError extends Error {
     super(message);
   }
 }
+
+const ErrorCodes = {
+  SKIP_ALLOWED_TYPE: () =>
+    new StageError("$skip: only integer values are allowed."),
+  SKIP_ALLOWED_RANGE: () =>
+    new StageError("$skip: only non negative integers are allowed."),
+  LIMIT_ALLOWED_TYPE: () =>
+    new StageError("$limit: only integer values are allowed."),
+  LIMIT_ALLOWED_RANGE: () =>
+    new StageError("$limit: only positive integers are allowed."),
+};
+
+export default ErrorCodes;
